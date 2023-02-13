59cbad1dbb0198f8376eca7d3de9e1af70f4cbb9
Veider <a.l.e.x.1.9@yandex.ru>
2023-02-13 03:09:31 +0300

    Sem 1 #1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿//Start
+﻿Console.WriteLine ("Введите число: ");
+int num1 = int.Parse (Console.ReadLine ())!;
+Console.WriteLine ("Введите число: ");
+int num2 = int.Parse (Console.ReadLine ())!;
+Console.WriteLine ("Введите число: ");
+int num3 = int.Parse (Console.ReadLine ())!;
+int max = num1;
+if (num2>max)
+    max = num2;
+    if (num3>max)
+        max = num3;
+if (num3>max)
+    max = num3;
+
+Console.WriteLine ("Max: " + max);
