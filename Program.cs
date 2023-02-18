68ba88a941b51bfa3cac72164f95c5a3bab5dd73
Veider <a.l.e.x.1.9@yandex.ru>
2023-02-19 00:52:05 +0300

    Список 2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,5 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число-день недели");
-int x = int.Parse(Console.ReadLine());
+﻿// Список по второму семинару
 
-if ((x<6) && (x>0))
-    {
-        Console.WriteLine ("Будний день");
-    }
-if ((x<8) && (x>5))
-    {
-        Console.WriteLine ("Выходной день");
-    }
-if ((x>=8) || (x<=0))
-    {
-        Console.WriteLine ("Такого дня не существует");
-    }
+// bfa0e51 Sem 2 #3
+// 6dd7eaa sem 2 #2
+// e343378 Sem2 #1
