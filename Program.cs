f3ca751e1dd7a4d9754f319fce0872d9758e2e4a
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-02 01:40:42 +0300

    Список 5

--- Program.cs
+++ Program.cs
@@ -1,35 +1,4 @@
-﻿Console.Clear();
-
-double fun(double [] mas)
-{
-    int i = 0;
-    int n = mas.Length;
-    for (i=0; i < n; i++){
-        mas[i] = new Random().NextDouble();
-        mas[i] = Math.Round (mas[i], 3);
-        Console.Write($"{mas[i]} ");}
-    return i;
-}
-
-double [] mas = new double [4];
-
-double kvest (double [] mas)
-{
-    int i = 0;
-    double min = mas [i];
-    double max = 0;
-    double sum = 0;
-    double n = mas.Length;
-    for (i=0; i < n; i++){
-        if (mas[i] < min)
-            min = mas [i];
-        if (mas[i] > max)
-            max = mas [i];}
-        sum = min + max;
-        Console.Write($"Сумма min + max: {sum}");
-    return i;
-}
-
-fun(mas);
-Console.WriteLine();
-kvest(mas);
+﻿// c094866  Sem 5 #38 в примере почему то не вещественные числа, 
+// хотя в условии нужно вроде для них решать
+// bf49dbf Sem 5 #36
+// 3c26228 Sem 5 #34
