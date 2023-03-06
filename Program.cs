f93440c6fb2c0d5d9946e7a641d9f44c7d9c2235
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-07 01:03:36 +0300

    Sem 6 #41 zapros 4isel

--- Program.cs
+++ Program.cs
@@ -1,4 +1,46 @@
-﻿// c094866  Sem 5 #38 в примере почему то не вещественные числа, 
-// хотя в условии нужно вроде для них решать
-// bf49dbf Sem 5 #36
-// 3c26228 Sem 5 #34
+﻿Console.Clear();
+
+int fun(string mes)
+{
+    Console.Write(mes);
+    string value = Console.ReadLine();
+    int res = int.Parse(value);
+    return res;
+}
+
+int[] input (int length)
+{
+    int [] mas = new int [length];
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = fun($"Vvod element({i + 1}) ");
+    }
+    return mas;
+}
+
+void print(int[]mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {mas[i]}");
+    }
+}
+
+int count(int[]mas)
+{
+    int count = 0;
+    for (int i = 0; i<mas.Length; i++)
+    {
+        if (mas[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = fun ("Vvod 4isel ");
+int [] mas;
+mas = input(length);
+print(mas);
+Console.WriteLine($"kol-vo 4isel > 0 ---> {count(mas)}");
