488c4b827831a77cc147f775158143d08ffd1bab
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-09 02:17:15 +0300

    Sem7 #47

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
-﻿Console.Clear();
+﻿using System;
+using static System.Console;
 
-Console.Write("vvod 4isla strok: ");
-int rows = int.Parse(Console.ReadLine());
-Console.Write("vvod 4isla stolbcov: ");
-int columns = int.Parse(Console.ReadLine());
-int[,] array = GetArray(rows, columns);
+Clear();
+
+int rows = 3;
+int columns = 4;
+double[,] array = GetArray(rows, columns);
 PrintArray(array);
-sum(array);
 
-int[,] GetArray(int m, int n)
+double[,] GetArray(int m, int n)
 {
-    int[,] result = new int[m, n];
+    double[,] result = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(0,11);
+            result[i, j] = Math.Round(new Random().NextDouble(), 2);
         }
     }
     return result;
 }
 
-void PrintArray(int[,] inArray)
+void PrintArray(double[,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
@@ -31,16 +31,4 @@ void PrintArray(int[,] inArray)
         }
         Console.WriteLine();
     }
-}
-
-int sum(int[,] array)
-{
-    int result = 0;
-    int length = array.GetLength(0) < array.GetLength(1) ? array.GetLength(1) : array.GetLength(0); // если условие слева выполняется, то(?), если нет то (:)
-    for (int i = 0; i < length; i++)
-    {
-        result += array[i,i]; 
-    }      
-    Console.WriteLine(result);
-    return result;
 }
