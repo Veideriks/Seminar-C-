a82f618a461fc311e33d61df18355c1eaafeb69c
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-14 06:57:35 +0300

    Sem 8 #56

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ int[,] array = GetArray(rows, columns);
 PrintArray(array);
 WriteLine("");
 kvest(array);
-PrintArray(array);
 
 int[,] GetArray(int m, int n)
 {
@@ -40,25 +39,25 @@ int [,] kvest (int [,] array)
 {
     int n = array.GetLength(0);
     int m = array.GetLength(1);
-    int swap = 0;
+    int sum = 0;
+    int sum2 = 0;
+    int min = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            for(int k = array.GetLength(1); k > 0 ; k--)
-            {
-                for(j = 0 ; j < m ; j++)
-                {
-                    if (j!=m-1 && array[i,j] < array[i,j+1])
-                    {
-                        swap = array[i,j];
-                        array[i,j] = array[i,j+1];
-                        array[i,j+1] = swap;
-                    }
-                }
-            }
+          sum = array[i,j]+sum;
         }
+        WriteLine($"{sum}");
+        
+        if (sum<sum2 || sum2==0)
+            {
+                min = i;
+                sum2 = sum;
+            }
+        sum = 0;
     }
     WriteLine("");
+    WriteLine($"Наименьшая сумма в строке № {min+1}");
     return array;
 }
