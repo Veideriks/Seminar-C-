fdfef2881f06b631cd435ca603880d0e76589527
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-14 06:57:00 +0300

    Sem 8 #54

--- Program.cs
+++ Program.cs
@@ -1,4 +1,64 @@
-﻿// 2a40680 Sem7 #Доп-
-// 5025a14 Sem7 #52
-// 81dbf36 Sem7 #50
-// 488c4b8 Sem7 #47
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int rows = 4;
+int columns = 4;
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+WriteLine("");
+kvest(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(0,5);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int [,] kvest (int [,] array)
+{
+    int n = array.GetLength(0);
+    int m = array.GetLength(1);
+    int swap = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for(int k = array.GetLength(1); k > 0 ; k--)
+            {
+                for(j = 0 ; j < m ; j++)
+                {
+                    if (j!=m-1 && array[i,j] < array[i,j+1])
+                    {
+                        swap = array[i,j];
+                        array[i,j] = array[i,j+1];
+                        array[i,j+1] = swap;
+                    }
+                }
+            }
+        }
+    }
+    WriteLine("");
+    return array;
+}
