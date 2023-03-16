a7ff6c57e21dd69ff48c6736e1a27eb1cb623398
Veider <a.l.e.x.1.9@yandex.ru>
2023-03-17 02:55:19 +0300

    Список 9

--- Program.cs
+++ Program.cs
@@ -1,18 +1,3 @@
-﻿using System;
-using static System.Console;
-
-Clear();
-
-int n = 3;
-int m = 2;
-
-WriteLine(kvest (n,m));
-
-int kvest (int n, int m)
-{
-    if (n==0)
-        return m+1;
-    else if (m==0)
-        return kvest (n-1, 1);
-    return kvest (n-1, kvest (n,m-1));
-}
+﻿// da8e0cc Sem9 #68
+// f727c43 Sem9 #64
+// 44900c0 Sem9 #66
